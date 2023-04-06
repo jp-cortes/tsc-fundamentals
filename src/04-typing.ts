303350c44c443541ab885285174a45264171e39f
Juan Cortes <alias99002@outlook.com>
2023-04-06 07:53:52 -0500

    first commit

---
+++ src/04-typing.ts
@@ -0,0 +1,13 @@
+// IIFE (Immediately Invoked Function Expression)
+(()=> {
+  let myProductName = 'Product 1';
+  let myProductPrice = 123;
+
+  myProductName = 'change';
+  myProductName.toLowerCase();
+
+  myProductPrice.toFixed();
+
+  const myProductStock = 1000;
+  const myProductName2 = 'Product 1';
+})();
