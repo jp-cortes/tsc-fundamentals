8f1f87314979701b8d597c0afe4b3cd0590e97c6
Juan Cortes <alias99002@outlook.com>
2023-04-08 08:58:00 -0500

    array, any, unionTypes, alias, undefined-null

---
+++ src/09-any.ts
@@ -0,0 +1,16 @@
+(()=>{
+  //there is no reson to use any only if you are learning
+  let myDynamicVar: any;
+  myDynamicVar = 100;
+  myDynamicVar = null;
+  myDynamicVar = {};
+  myDynamicVar = '';
+
+  myDynamicVar = 'Hola';
+  const rta = (myDynamicVar as string).toLowerCase();
+  console.log(rta);
+
+  myDynamicVar = 1212;
+  const rta2 = (<number>myDynamicVar).toFixed();
+  console.log(rta2);
+})();
